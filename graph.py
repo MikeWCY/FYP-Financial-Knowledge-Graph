1da26785d77dc5794daa124b2b7ea6a6e6769cf1
Mike WANG Chenyu <45159881+MikeWCY@users.noreply.github.com>
2021-11-10 16:27:56 +0800

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,38 @@
+from py2neo import Graph
+
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "123456"))
+
+class Connect():
+    def __init__(self):
+        self.query = ""
+
+    def run_query(self):
+        result = graph.run(self.query)
+        ls = []
+        result.forward()
+        if type(result.current[0]) == str:
+            ls.append(result.current[0])
+        else:
+            ls.append(result.current[0]["name"])
+            while result.forward():
+                if result.current[0]:
+                    ls.append(result.current[0]["name"])
+        return ls
+
+    def get_company_list(self):
+        query = "MATCH (c: Institution) RETURN c"
+        ls = []
+        result = graph.run(query)
+        while result.forward():
+            if result.current[0]:
+                ls.append(result.current[0]["name"])
+        return ls
+
+    def get_service_list(self):
+        query = "MATCH (c: Service) RETURN c"
+        ls = []
+        result = graph.run(query)
+        while result.forward():
+            if result.current[0]:
+                ls.append(result.current[0]["name"])
+        return ls
