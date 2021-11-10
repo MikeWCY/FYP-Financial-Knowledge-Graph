ff7f616ead668aedc803e71e4c6810a3ea8ab078
Mike WANG Chenyu <45159881+MikeWCY@users.noreply.github.com>
2021-11-10 21:24:09 +0800

    Update ask.py

--- ask.py
+++ ask.py
@@ -6,6 +6,8 @@ s3 = "what services does the parent company of Bank of China (Hong Kong) provide
 s4 = "what is the type of Bank of China (Hong Kong)"
 s5 = "what is the type of the parent company of Bank of China (Hong Kong)"
 q = Question(s3)
-# run_simple() is used to process questions with one triple and run_complex() used to process questions with more than one triples
+# run_simple() is used to process questions with one triple
+# run_complex() is used to process questions with more than one triples
+# run_complex_type() utilizes the type-based question decomposition method to achieve a high efficiency
 # the limit of run_complex() is 3
 print(q.run_complex())
