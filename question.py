304106dbc2c25a74c95b024ca5dbc3a5f7683091
Mike WANG Chenyu <45159881+MikeWCY@users.noreply.github.com>
2021-11-20 18:00:01 +0800

    Update question.py

--- question.py
+++ question.py
@@ -1,14 +1,16 @@
 from graph import Connect
 import textdistance
 import pandas as pd
+from operator import length_hint
 
 
 class Question():
     def __init__(self, sent):
         self.sent = sent
+        self.process_question()
         self.df = pd.read_csv("template.csv")
         self.connect = Connect()
-        self.data = {"<Institution>": [], "<Service>": []}
+        self.data = {"<Institution>": [], "<Service>": [], "<string>": []}
         self.data["<Institution>"] = self.connect.get_company_list()
         self.data["<Service>"] = self.connect.get_service_list()
         self.pattern = sent
@@ -17,30 +19,58 @@ class Question():
         self.candidate_entityType = []
         self.query = ""
         self.result = ""
-
         self.decomposed_qs = []
         self.query_ls = []
         self.answerType_ls = []
+    
+    def process_question(self):
+        self.sent = self.sent.replace("that", "")
+        words = self.sent.split(" ")
+        if "which" in self.sent and words[0] != "which":
+            self.sent = self.sent[0] + self.sent[1:].replace("which", "")
+        if "who" in self.sent and words[0] != "who":
+            self.sent = self.sent[0] + self.sent[1:].replace("who", "")
         
     @staticmethod
     def get_entityType(data, sent):
         entity = None
         entityType = None
         pattern = None
+        recognized_entities = []
+        recognized_type = None
         for k in data.keys():
-            if k in sent:
-                entity = k
-                entityType = k
-                pattern = sent
-                continue
             for c in data[k]:
                 if c in sent:
-                    pattern = sent.replace(c, k)
+                    recognized_entities.append(c)
+                    recognized_type = k
+                    pattern = sent
+                    #pattern = sent.replace(c, k)
                     entity = c
                     entityType = k
                     break
+        if len(recognized_entities) != 0:
+            max_len = 0
+            max_entity = ""
+            for e in recognized_entities:
+                length = len(e)
+                if length >= max_len:
+                    max_len = length
+                    max_entity = e
+        if entity != None:
+            for k in data.keys():
+                if k in sent:
+                    pattern = sent.replace(k, "")
+                    sent = sent.replace(k, "")
+            pattern = pattern.replace(max_entity, recognized_type)
+        else:
+            for k in data.keys():
+                if k in sent:
+                    entity = k
+                    entityType = k
+                    pattern = sent
+        
         #print([sent, entity, entityType, pattern])
-        return [entity, entityType, pattern]
+        return [entity, entityType, sent, pattern]
 
     @staticmethod
     def find_all_entities(data, sent):
@@ -85,13 +115,14 @@ class Question():
         min_index = -1
         for j in range(len(pattern_ls)):
             distance = textdistance.damerau_levenshtein(pattern, pattern_ls[j])
+            #distance2 = textdistance.damerau_levenshtein(pattern, pattern_ls[j].replace("what ", ""))
             if distance < min_distance:
                 min_distance = distance
                 min_index = j
             #print(pattern_ls[j], distance)
         if min_distance == 1000 and min_index == -1:
             return [None, None]
-        elif min_distance < 10:
+        elif min_distance < 8:
             matched_pattern = pattern_ls[min_index]
             query = lquery_ls[min_index] + entity + rquery_ls[min_index]
             answerType = answerType_ls[min_index]
@@ -108,7 +139,7 @@ class Question():
             for j in range(i + 1, len(word_ls) + 1):
                 sub = word_ls[i:j]
                 subq = blank.join(sub)
-                [entity, entityType, pattern] = self.get_entityType(self.data, subq)
+                [entity, entityType, subq, pattern] = self.get_entityType(self.data, subq)
                 if entity ==  None or entityType == None or pattern == None:
                     continue
                 [query, answerType] = self.match_pattern(self.df, entity, entityType, pattern)
@@ -116,6 +147,13 @@ class Question():
                     continue
                 newsent = sent.replace(subq, answerType)
                 newsent_ls = newsent.split(" ")
+                '''
+                print(subq, "----", newsent)
+                [entity, entityType, s, pattern] = self.get_entityType(self.data, newsent)
+                #print(newsent, entity)
+                #print(self.match_pattern(self.df, entity, entityType, pattern)[0])
+                print("-"*40)
+                '''
                 if len(newsent_ls) == 1 or self.question_decomposition(newsent, depth + 1) != [None, None, None]:
                     self.decomposed_qs.append(subq)
                     self.query_ls.append(query)
@@ -126,24 +164,31 @@ class Question():
     def run_query(self, query):
         # print(query)
         self.connect.query = query
-        result = self.connect.run_query()
+        result = self.connect.run()
         return result
 
     def run_simple(self):
-        [self.entity, self.entityType, self.pattern] = self.get_entityType(self.data, self.sent)      
-        [self.query, answerType] = self.match_pattern(self.df, self.entity, self.entityType, self.pattern)
+        [self.entity, self.entityType, s, self.pattern] = self.get_entityType(self.data, self.sent)      
+        [self.query, self.answerType] = self.match_pattern(self.df, self.entity, self.entityType, self.pattern)
+        #print(self.pattern)
         self.result = self.run_query(self.query)
-        return self.result
-
+        if self.result == None:
+            return "no result"
+        else:
+            return self.result
+        
     def run_complex(self):
         self.decomposed_qs = []
         self.query_ls = []
         self.question_decomposition(self.sent, 0)
         result = []
+        print("D:", self.decomposed_qs)
         for i in range(len(self.decomposed_qs) - 1, -1, -1):
             r = self.run_query(self.query_ls[i])
+            if r == None:
+                return "no result"
             result.append(r)
-            if i > 0 :
+            if i > 0:
                 for j in r:
                     self.query_ls[i - 1] = self.query_ls[i - 1].replace(self.answerType_ls[i], j)
         if len(result) == 0:
@@ -181,8 +226,11 @@ class Question():
         self.query_ls = []
         self.type_question_decomposition(self.sent, 0)
         result = []
+        print("D:", self.decomposed_qs)
         for i in range(len(self.decomposed_qs) - 1, -1, -1):
             r = self.run_query(self.query_ls[i])
+            if r == None:
+                return "no result"
             result.append(r)
             if i > 0 :
                 for j in r:
