db15a50f3e4e1e9f8356c37cd1868a1b5f56ecd6
Mike WANG Chenyu <45159881+MikeWCY@users.noreply.github.com>
2021-11-20 17:59:30 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,7 @@
+import re
 from py2neo import Graph
+import json
+from flask import render_template
 
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "123456"))
 
@@ -6,18 +9,24 @@ class Connect():
     def __init__(self):
         self.query = ""
 
+    def run(self):
+        return graph.run(self.query).data()
+
     def run_query(self):
         result = graph.run(self.query)
         ls = []
-        result.forward()
-        if type(result.current[0]) == str:
-            ls.append(result.current[0])
-        else:
-            ls.append(result.current[0]["name"])
-            while result.forward():
-                if result.current[0]:
-                    ls.append(result.current[0]["name"])
-        return ls
+        try:
+            result.forward()
+            if type(result.current[0]) == str:
+                ls.append(result.current[0])
+            else:
+                ls.append(result.current[0]["name"])
+                while result.forward():
+                    if result.current[0]:
+                        ls.append(result.current[0]["name"])
+            return ls
+        except TypeError:
+            return None
 
     def get_company_list(self):
         query = "MATCH (c: Institution) RETURN c"
@@ -36,3 +45,4 @@ class Connect():
             if result.current[0]:
                 ls.append(result.current[0]["name"])
         return ls
+    
